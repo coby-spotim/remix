2d87701349430a629e6d171acbb409e1493620d8
Coby Benveniste <coby.b@spot.im>
2018-07-31 13:18:51 +0300

    Fixed warnings in mix.exs, added .formatter.exs,  updated elixir version to 1.6, updated description, updated GitHub link, updated maintainers

--- mix.exs
+++ mix.exs
@@ -2,34 +2,36 @@ defmodule Remix.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :remix,
-     version: "0.0.2",
-     elixir: "~> 1.0",
-     package: package,
-     description: description,
-     deps: deps]
-  end
-
-  def application do
-    [applications: [:logger],
-     mod: {Remix, []}]
-  end
-
-  defp deps, do: []
-
-  defp package do
     [
-      licenses: ["Apache 2.0"],
-      maintainers: ["Alan Peabody", "Mike Westbom", "Jordan Morano", "Brendan Fey"],
+      app: :remix,
+      version: "0.0.2",
+      elixir: "~> 1.6",
+      package: package(),
+      description: description(),
+      deps: deps()
+    ]
+  end
+
+  def application() do
+    [applications: [:logger], mod: {Remix, []}]
+  end
+
+  defp deps(), do: []
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      maintainers: ["Coby Benveniste"],
       links: %{
-        "GitHub" => "https://github.com/AgilionApps/remix"
+        "GitHub" => "https://github.com/coby-spotim/remix"
       }
     ]
   end
 
-  defp description do
+  defp description() do
     """
-    Recompiles mix projects on any change to the lib directory.
+    A fork of the original remix package which fixes several errors from the old version, which hasn't been updated since 2016.
+    Original Description: Recompiles mix projects on any change to the lib directory.
     """
   end
 end
